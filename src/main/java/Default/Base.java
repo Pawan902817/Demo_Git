3f917ec8a2f9107c650e1e2b0680f568132772d8
Pawan <Pawanrewatkar28@gmail.com>
2022-08-14 19:12:56 +0530

    First commit...

---
+++ src/main/java/Default/Base.java
@@ -0,0 +1,11 @@
+package Default;
+
+public class Base {
+int x=10,y=20;
+
+public void m1() {
+	int add = x + y;
+	System.out.println(add);
+}
+	
+}
---
+++ src/test/java/test/Test1.java
@@ -0,0 +1,13 @@
+package test;
+
+import Default.Base;
+
+public class Test1 {
+public static void main(String[] args) {
+	
+	Base b = new Base();
+	b.m1();
+	
+}
+	
+}
